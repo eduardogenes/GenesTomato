9a093b47e2880b3f1c95fecee404f9ee2b86bf06
Eduardo Genes <eduardogenes95@gmail.com>
2025-07-18 17:09:45 -0300

    feat: Integrate components and apply global styles
    Monta a tela principal da aplicação no `App.tsx` utilizando todos os novos componentes e aplica os estilos globais e o tema de cores.
    
    - Integra `Logo`, `Menu`, `CountDown`, `DefaultInput` e `Cycles` no `App.tsx`.
    - Define as variáveis de cores no `theme.css` e os estilos base no `global.css`.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,56 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+
 import './styles/global.css';
 import './styles/theme.css';
-import { TimerIcon } from 'lucide-react';
-import { Heading } from './components/Heading';
-
+import { PlayIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        GenesTomato 
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <div>
-        <p>00:00:00</p>
-      </div>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              id='meuInput'
+              labelText='task'
+              type='text'
+              placeholder='Digite a tarefa'
+              disabled
+            />
+          </div>
+
+          <div className='formRow'>
+            <p>Nesse ciclo foque por 25 minutos</p>
+          </div>
+
+          <div className='formRow'>
+            <Cycles />
+          </div>
+
+          <div className='formRow'>
+            <button type='submit' className='buttonForm'>
+              <PlayIcon />
+            </button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
